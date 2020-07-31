1a955067bb3c08882ef663c77157a78b9458ac35
kdzapp <kdzapp@umich.edu>
2020-07-31 10:17:56 -0700

    Socket Connected

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,159 +1,146 @@
-navigator.getUserMedia(
-    { video: true, audio: true },
-    stream => {
-        const localVideo = document.getElementById("local-video");
-        if (localVideo) {
-            localVideo.srcObject = stream;
-        }
-    },
-    error => {
-        console.warn(error.message);
-    }
-);
+let isAlreadyCalling = false;
+let getCalled = false;
 
-this.io("connection", socket => { // cant use this.io.on bc it is not callable
-    const existingSocket = this.activeSockets.find(
-        existingSocket => existingSocket === socket.id
-    );
+const existingCalls = [];
 
-    if (!existingSocket) {
-        this.activeSockets.push(socket.id);
+const { RTCPeerConnection, RTCSessionDescription } = window;
 
-        socket.emit("update-user-list", {
-            users: this.activeSockets.filter(
-                existingSocket => existingSocket !== socket.id
-            )
-        });
+const peerConnection = new RTCPeerConnection();
 
-        socket.broadcast.emit("update-user-list", {
-            users: [socket.id]
-        });
-    }
-})
+function unselectUsersFromList() {
+  const alreadySelectedUser = document.querySelectorAll(
+    ".active-user.active-user--selected"
+  );
 
-socket.on("disconnect", () => { // error here bc socket falls out of scope
-    this.activeSockets = this.activeSockets.filter(
-        existingSocket => existingSocket !== socket.id
-    );
-
-    socket.broadcast.emit("remove-user", {
-        socketId: socket.id
-    });
-});
-
-socket.on("update-user-list", ({ users }) => {
-    updateUserList(users);
-   }),
-    
-    socket.on("remove-user", ({ socketId }) => {
-        const elToRemove = document.getElementById(socketId);
-    
-    if (elToRemove) {
-        elToRemove.remove();
-    }
-});
-
-function updateUserList(socketIds) {
-    const activeUserContainer = document.getElementById("active-user-container");
-    
-    socketIds.forEach(socketId => {
-        const alreadyExistingUser = document.getElementById(socketId);
-        if (!alreadyExistingUser) {
-            const userContainerEl = createUserItemContainer(socketId);
-            activeUserContainer.appendChild(userContainerEl);
-        }
-    });
+  alreadySelectedUser.forEach((el) => {
+    el.setAttribute("class", "active-user");
+  });
 }
 
 function createUserItemContainer(socketId) {
-    const userContainerEl = document.createElement("div");
-    
-    const usernameEl = document.createElement("p");
-    
-    userContainerEl.setAttribute("class", "active-user");
-    userContainerEl.setAttribute("id", socketId);
-    usernameEl.setAttribute("class", "username");
-    usernameEl.innerHTML = `Socket: ${socketId}`;
-    
-    userContainerEl.appendChild(usernameEl);
-    
-    userContainerEl.addEventListener("click", () => {
-        unselectUsersFromList();
-        userContainerEl.setAttribute("class", "active-user active-user--selected");
-        const talkingWithInfo = document.getElementById("talking-with-info");
-        talkingWithInfo.innerHTML = `Talking with: "Socket: ${socketId}"`;
-        callUser(socketId);
-    });
+  const userContainerEl = document.createElement("div");
 
-    return userContainerEl;
+  const usernameEl = document.createElement("p");
+
+  userContainerEl.setAttribute("class", "active-user");
+  userContainerEl.setAttribute("id", socketId);
+  usernameEl.setAttribute("class", "username");
+  usernameEl.innerHTML = `Socket: ${socketId}`;
+
+  userContainerEl.appendChild(usernameEl);
+
+  userContainerEl.addEventListener("click", () => {
+    unselectUsersFromList();
+    userContainerEl.setAttribute("class", "active-user active-user--selected");
+    const talkingWithInfo = document.getElementById("talking-with-info");
+    talkingWithInfo.innerHTML = `Talking with: "Socket: ${socketId}"`;
+    callUser(socketId);
+  });
+
+  return userContainerEl;
 }
 
 async function callUser(socketId) {
-    const offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
-    
-    socket.emit("call-user", {
-        offer,
-        to: socketId
-    });
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
+
+  socket.emit("call-user", {
+    offer,
+    to: socketId,
+  });
 }
 
-socket.on("call-user", data => {
-    socket.to(data.to).emit("call-made", {
-        offer: data.offer,
-        socket: socket.id
-    });
-});
+function updateUserList(socketIds) {
+  const activeUserContainer = document.getElementById("active-user-container");
 
-socket.on("call-made", async data => {
-    await peerConnection.setRemoteDescription(
-        new RTCSessionDescription(data.offer)
-    );
-    const answer = await peerConnection.createAnswer();
-    await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
-    
-    socket.emit("make-answer", {
-        answer,
-        to: data.socket
-    });
-});
+  socketIds.forEach((socketId) => {
+    const alreadyExistingUser = document.getElementById(socketId);
+    if (!alreadyExistingUser) {
+      const userContainerEl = createUserItemContainer(socketId);
 
-socket.on("make-answer", data => {
-    socket.to(data.to).emit("answer-made", {
-        socket: socket.id,
-        answer: data.answer
-    });
-});
-
-socket.on("answer-made", async data => {
-    await peerConnection.setRemoteDescription(
-        new RTCSessionDescription(data.answer)
-    );
-    
-    if (!isAlreadyCalling) {
-        callUser(data.socket);
-        isAlreadyCalling = true;
+      activeUserContainer.appendChild(userContainerEl);
     }
+  });
+}
+
+const socket = io.connect("localhost:5000");
+
+socket.on("update-user-list", ({ users }) => {
+  updateUserList(users);
 });
+
+socket.on("remove-user", ({ socketId }) => {
+  const elToRemove = document.getElementById(socketId);
+
+  if (elToRemove) {
+    elToRemove.remove();
+  }
+});
+
+socket.on("call-made", async (data) => {
+  if (getCalled) {
+    const confirmed = confirm(
+      `User "Socket: ${data.socket}" wants to call you. Do accept this call?`
+    );
+
+    if (!confirmed) {
+      socket.emit("reject-call", {
+        from: data.socket,
+      });
+
+      return;
+    }
+  }
+
+  await peerConnection.setRemoteDescription(
+    new RTCSessionDescription(data.offer)
+  );
+  const answer = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
+
+  socket.emit("make-answer", {
+    answer,
+    to: data.socket,
+  });
+  getCalled = true;
+});
+
+socket.on("answer-made", async (data) => {
+  await peerConnection.setRemoteDescription(
+    new RTCSessionDescription(data.answer)
+  );
+
+  if (!isAlreadyCalling) {
+    callUser(data.socket);
+    isAlreadyCalling = true;
+  }
+});
+
+socket.on("call-rejected", (data) => {
+  alert(`User: "Socket: ${data.socket}" rejected your call.`);
+  unselectUsersFromList();
+});
+
+peerConnection.ontrack = function ({ streams: [stream] }) {
+  const remoteVideo = document.getElementById("remote-video");
+  if (remoteVideo) {
+    remoteVideo.srcObject = stream;
+  }
+};
 
 navigator.getUserMedia(
-    { video: true, audio: true },
-    stream => {
-        const localVideo = document.getElementById("local-video");
-        if (localVideo) {
-            localVideo.srcObject = stream;
-        }
-    
-        stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
-    },
-    error => {
-        console.warn(error.message);
+  { video: true, audio: true },
+  (stream) => {
+    const localVideo = document.getElementById("local-video");
+    if (localVideo) {
+      localVideo.srcObject = stream;
     }
-);
 
-peerConnection.ontrack = function({ streams: [stream] }) {
-    const remoteVideo = document.getElementById("remote-video");
-    if (remoteVideo) {
-        remoteVideo.srcObject = stream;
-    }
-};
+    stream
+      .getTracks()
+      .forEach((track) => peerConnection.addTrack(track, stream));
+  },
+  (error) => {
+    console.warn(error.message);
+  }
+);
